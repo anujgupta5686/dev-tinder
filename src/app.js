6e559940f006fa7384e494030148253484eb1de9
anujgupta5686 <anujgupta5686@gmail.com>
2024-10-08 16:40:39 +0530

    Added homework code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.send("Server Started");
+app.use("/", (req, res) => {
+  res.send("Hello Anuj Kumar Gupta!");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello Route!");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!");
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
